d60c757eb931be55931c2e7932c191c9fa8832cf
antoniozacca <antoniozaccaria057@gmail.com>
2025-07-02 18:56:51 +0200

    add mode background

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,30 +1,37 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useCart } from "../context/CartContext";
-import { useWishlist } from "../context/WishlistContext"; // import wishlist context
+import { useWishlist } from "../context/WishlistContext";
+import { useTheme } from "../context/ThemeContext"; // <-- Importa il context del tema
 import { Link } from "react-router-dom";
 
 function Navbar() {
   const { user, logout } = useContext(AuthContext);
-  const { cart, setCart } = useCart(); // aggiungo setCart per pulire
-  const { wishlist } = useWishlist();  // prendo la wishlist
+  const { cart, setCart } = useCart();
+  const { wishlist } = useWishlist();
+  const { theme, toggleTheme } = useTheme(); // <-- Usa lo switch del tema
 
   const handleLogout = () => {
     logout();
-    setCart([]); // pulisce il carrello al logout
+    setCart([]);
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <div className="container-fluid">
         <Link className="navbar-brand" to="/">MyStore</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto">
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto align-items-center">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
             </li>
 
-            {!user && (
+            {!user ? (
               <>
                 <li className="nav-item">
                   <Link className="nav-link" to="/registrazione">Registrati</Link>
@@ -33,37 +40,34 @@ function Navbar() {
                   <Link className="nav-link" to="/login">Login</Link>
                 </li>
               </>
-            )}
-
-            {user && (
+            ) : (
               <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cart">Carrello ({cart.length})</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/wishlist">WishList ({wishlist.length})</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/add-product">Aggiungi Prodotto</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/my-products">I miei prodotti</Link>
+                </li>
                 <li className="nav-item">
                   <button className="btn btn-link nav-link" onClick={handleLogout}>
                     Logout
                   </button>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/cart">
-                    Carrello ({cart.length})
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/wishlist">
-                    WishList ({wishlist.length})
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/add-product">
-                    Aggiungi Prodotto
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/my-products">
-                    I miei prodotti
-                  </Link>
-                </li>
               </>
             )}
+
+            {/* Tema Light/Dark visibile a tutti */}
+            <li className="nav-item ms-3">
+              <button className="btn btn-outline-light btn-sm" onClick={toggleTheme}>
+                {theme === 'light' ? '🌙 Tema scuro' : '☀️ Tema chiaro'}
+              </button>
+            </li>
           </ul>
         </div>
       </div>
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -6,13 +6,16 @@ import { CartProvider } from './context/CartContext'
 import { WishlistProvider } from './context/WishlistContext'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { ThemeProvider } from './context/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
         <WishlistProvider>
-          <App />
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
         </WishlistProvider>
       </CartProvider>
     </BrowserRouter>
